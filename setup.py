830b07bed459b34918f5a9c7ee33f8b14c34eb78
B. Arman Aksoy <arman@aksoy.org>
2015-11-09 18:37:10 -0500

    reformat setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup
 
-config = {
-    'description': 'Pileup',
-    'author': 'B. Arman Aksoy',
-    'url': 'https://github.com/armish/pileup',
-    'download_url': 'https://github.com/armish/pileup/releases',
-    'author_email': 'arman@aksoy.org',
-    'license': 'http://www.apache.org/licenses/LICENSE-2.0.html',
-    'version': '0.0.1',
-    'install_requires': [
-        'nose>=1.3.7',
-        'urwid>=1.3.1',
-        'twobitreader>=3.1.0',
-        'pysam>=0.8.3'],
-    'packages': ['pileup'],
-    'scripts': [],
-    'name': 'pileup'
-}
-
-setup(**config)
+setup(description = 'Pileup',
+      author = 'B. Arman Aksoy',
+      url = 'https://github.com/armish/pileup',
+      download_url = 'https://github.com/armish/pileup/releases',
+      author_email = 'arman@aksoy.org',
+      license = 'http://www.apache.org/licenses/LICENSE-2.0.html',
+      version = '0.0.1',
+      install_requires = [
+           'nose>=1.3.7',
+           'urwid>=1.3.1',
+           'twobitreader>=3.1.0',
+           'pysam>=0.8.3'],
+      packages = ['pileup', 'pileup'],
+      name = 'pileup',
+      entry_points = {'console_scripts': ['pileup=pileup.cli:main']}
+)
